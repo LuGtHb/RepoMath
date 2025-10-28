0a4fdf7eff8a4cc206c2f4a4545ee1a1f7057ac5
LuGtHb <lucia.salgado@iesdoctorbalmis.com>
2025-10-28 13:52:18 +0100

    Factorial

--- LibreriaMath/Matematicas.cs
+++ LibreriaMath/Matematicas.cs
@@ -11,5 +11,16 @@
             }
             return suma;
         }
+
+
+        public static int Factorial(int n)
+        {
+            int resultado = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                resultado *= i;
+            }
+            return resultado;
+        }
     }
 }
